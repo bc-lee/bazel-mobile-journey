b8389310fba079d54dc614a90ba63aeecd525474
Byoungchan Lee <byoungchan.lee@gmx.com>
2025-03-29 22:18:29 +0900

    Add initial setup for Simple iOS App with Bazel support

---
+++ ios/simple_swift_app/Sources/ViewController.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Simple Swift App"
+        label.font = UIFont.boldSystemFont(ofSize: 24)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+    }
+
+    private func setupUI() {
+        view.backgroundColor = .white
+        title = "Home"
+
+        view.addSubview(titleLabel)
+
+        NSLayoutConstraint.activate([
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+        ])
+    }
+}
